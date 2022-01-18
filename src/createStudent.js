e1e1c6112ea0428784a7866d9e4d3e1d159d3588
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-18 02:50:03 -0300

    Corrigidos erros do lint requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,9 +16,7 @@
 */
 
 const createStudent = (name) => {
-  const feedback = () => { 
-    return `Eita pessoa boa!`; 
-  }   
+  const feedback = () => 'Eita pessoa boa!';      
   return {
     name,
     feedback,    
