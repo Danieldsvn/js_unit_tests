7e7a161998d725fb3b0ac04140ba379932ce32d9
Daniel Saraiva <danieldsvn@gmail.com>
2022-01-24 03:41:58 -0300

    Requisito 6 agora com 6 testes feitose REALMENTE concluido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,11 +35,11 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se o retorno da função é um array.
     expect(productDetails()).toHaveLength(2);
     // Teste se o array retornado pela função contém dois itens dentro.
-    expect(typeof productDetails()[0]).toBe('object');
-    expect(typeof productDetails()[1]).toBe('object');
+    expect(typeof productDetails()[0] === 'object' && typeof productDetails()[1] === 'object').toBeTruthy();    
     // Teste se os dois itens dentro do array retornado pela função são objetos.
-    
+    expect(productDetails('arroz', 'feijão')[0] !==  productDetails('arroz', 'feijão')[1]).toBeTruthy(); 
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails()[0].details.productId.endsWith('123') && productDetails()[1].details.productId.endsWith('123')).toBeTruthy(); 
     // Teste se os dois productIds terminam com 123.
   });
 });
